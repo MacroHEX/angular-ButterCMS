02f2ac29b3061f0ef480b9b33a28840612cf6b87
MacroHEX <martin18py@gmail.com>
2023-09-30 01:22:41 -0400

    Añido code highlight

--- src/mods/frames/panels/frame-panel/frame-panel.component.ts
+++ src/mods/frames/panels/frame-panel/frame-panel.component.ts
@@ -1,7 +1,10 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, Inject, OnInit} from '@angular/core';
 import {AppService} from "../../../../app/services/app.service";
 import {APP_DTO, APP_PARENT} from "../../../../base/base.module";
 import {ContentFrame} from "../../../../base/frames/api/content.frame";
+import * as Prism from 'prismjs';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-scss';
 
 @Component({
   selector: 'app-frame-panel',
@@ -15,16 +18,23 @@ export class FramePanelComponent implements OnInit {
   // ::: vars
   //
   title = '';
+  htmlCode: string = '';
+  tsCode: string = '';
+  scssCode: string = '';
 
   // ::: constructor
   //
   constructor(private appService: AppService,
+              private cdRef: ChangeDetectorRef,
               @Inject(APP_PARENT) protected parent: ContentFrame,
               @Inject(APP_DTO) protected data: any) {
   }
 
   // ::: init
   //
+  ngAfterViewInit() {
+    this.highlight();
+  }
 
   // ::: ng
   //
@@ -41,8 +51,25 @@ export class FramePanelComponent implements OnInit {
     this.appService.butter().pages().get(this.data)
       .onSuccess(msg => {
         this.title = msg.data.data.fields.frames.title;
+        this.htmlCode = msg.data.data.fields.frames.html_code;
+        this.tsCode = msg.data.data.fields.frames.ts_code;
+        this.scssCode = msg.data.data.fields.frames.css_code;
+
+        // Tell Angular to check for updates
+        this.cdRef.detectChanges();
+
+        // Now highlight the code
+        this.highlight();
       })
       .call();
+  }
+
+  highlight() {
+    setTimeout(() => {
+      document.querySelectorAll('pre code').forEach((block) => {
+        Prism.highlightElement(block);
+      });
+    }, 0);
   }
 
 }
